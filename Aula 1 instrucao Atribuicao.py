1a91de2e4fc0a87e2838fb3541fb74dda7fabe7b
Renan479 <renangodoy79@gmail.com>
2026-03-09 11:12:35 -0300

    Add files via upload

--- Aula 1 instrucao Atribuicao.py
+++ Aula 1 instrucao Atribuicao.py
@@ -1,9 +1,11 @@
 #Variavel é um espaço de memoria que
 #tem um nome e guarda algum dado do meu programa
-nome = 1
-sobrenome = numero + numero + 5
-
+numero = 1
+#Expressoes sao quaisquer combinações de
+# valor, variável e operadores
+resultado = (numero + numero) + 5
+resultado = (numero * (numero * numero) + 33)
 print(numero)
-
+print(resultado)
 
 
