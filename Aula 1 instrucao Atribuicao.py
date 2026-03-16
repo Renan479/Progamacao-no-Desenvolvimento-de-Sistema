acf59f800eba500bd94e5a18ef777058fcb2bea1
Renan479 <renangodoy79@gmail.com>
2026-03-16 11:25:17 -0300

    aula 1

--- Aula 1 instrucao Atribuicao.py
+++ Aula 1 instrucao Atribuicao.py
@@ -3,9 +3,30 @@
 numero = 1
 #Expressoes sao quaisquer combinações de
 # valor, variável e operadores
+
 resultado = (numero + numero) + 5
+
 resultado = (numero * (numero * numero) + 33)
+
 print(numero)
+
+# A instrução print mostra o  programa no terminal
+
 print(resultado)
 
+# Precedencia de operadores
+# Parenteses precendencia mais alta
+# Exponenciação
+# Multiplicação e a Divisão
+# Adição e a Subtração
+
+# Concatenação de Strings
+nome = 'Jose'
+sobrenome = 'Valim'
+nomeInteiro = nome + sobrenome
+print(nomeInteiro)
+
+# Multiplicado de Strings
+string1 = 'teste' * 3
+print(string1)
 
